47e7368be712c02937960331b1f7df7495f3fe03
Krzysztof Caban <kcaban99@gmail.com>
2024-08-24 16:20:47 +0200

    Added implementation of methods in Wall class

--- src/main/java/kc/Wall.java
+++ src/main/java/kc/Wall.java
@@ -1,28 +1,57 @@
 package kc;
 
 import kc.interfaces.Block;
+import kc.interfaces.CompositeBlock;
 import kc.interfaces.Structure;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Wall implements Structure {
+    private List<Block> blocks;
+
+    public Wall(List<Block> blocks) {
+        if (blocks == null) {
+            throw new IllegalArgumentException("Blocks list cannot be null");
+        }
+        this.blocks = blocks;
+    }
 
     @Override
     public Optional<Block> findBlockByColor(String color) {
-        //TODO: implement
-        return Optional.empty();
+        if (color == null) {
+            throw new IllegalArgumentException("Color cannot be null");
+        }
+        return flatMapBlocks(blocks.stream())
+                .filter(block -> block.getColor().equals(color))
+                .findFirst();
     }
 
     @Override
     public List<Block> findBlocksByMaterial(String material) {
-        //TODO: implement
-        return List.of();
+        if (material == null) {
+            throw new IllegalArgumentException("Material cannot be null");
+        }
+        return flatMapBlocks(blocks.stream())
+                .filter(block -> block.getMaterial().equals(material))
+                .collect(Collectors.toList());
     }
 
     @Override
     public int count() {
-        //TODO: implement
-        return 0;
+        return (int) flatMapBlocks(blocks.stream()).count();
+    }
+
+    private Stream<Block> flatMapBlocks(Stream<Block> blockStream) {
+        return blockStream.flatMap(block -> {
+            if (block instanceof CompositeBlock) {
+                return flatMapBlocks(((CompositeBlock) block).getBlocks().stream());
+            } else {
+                return Stream.of(block);
+            }
+        });
     }
 }
